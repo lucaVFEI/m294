4700dea9f953697456b3d212295b557185b9e789
Luca Reinstadler <lucareinstadler@gmail.com>
2023-02-06 14:34:53 +0100

    first commit

---
+++ Components/Sidebar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+/* Import library für Sidebar */
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+
+/* Import für Router library */
+import { NavLink } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+
+/* Seiten Importieren */
+import Kurse from '../Sites/Kurse/Dashboard';
+import KurseEdit from '../Sites/Kurse/KurseEdit';
+import KurseAdd from '../Sites/Kurse/KurseAdd';
+import Home from '../Sites/Home';
+
+/* Hier wird die Sidebar erstellt. Ebenso wird hier der Router für die Seiten erstellt. Alle neuen Routen müssen hier definiert werden.*/
+const Sidebar = () => {
+   return (
+    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial'}}>
+      <CDBSidebar textColor="#fff" backgroundColor="#333">
+        <CDBSidebarHeader>
+          <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
+            Menu
+          </a>
+        </CDBSidebarHeader>
+
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+            <NavLink exact to="/" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/kurse" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="columns">Kurse</CDBSidebarMenuItem>
+            </NavLink>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+      </CDBSidebar>
+      <Routes>
+         <Route exact path="/" element={<Home />} />
+         <Route exact path="/kurse" element={<Kurse />} />
+         <Route path="/kurse/edit/:id" element={<KurseEdit />} />
+         <Route path="/kurse/add/" element={<KurseAdd />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Sidebar;
